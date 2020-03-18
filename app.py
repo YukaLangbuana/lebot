4bdbac7512f8f71dec924576e529279dd1c85049
YukaLangbuana <langbuana.yuka@hotmail.com>
2020-03-18 11:30:28 +0700

    another attempt on deployment

--- app.py
+++ app.py
@@ -1,45 +1,28 @@
 import os
 from decouple import config
-from flask import (
-    Flask, request, abort
-)
-from linebot import (
-    LineBotApi, WebhookHandler
-)
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
 app = Flask(__name__)
 # get LINE_CHANNEL_ACCESS_TOKEN from your environment variable
-line_bot_api = LineBotApi(
-    config("LINE_CHANNEL_ACCESS_TOKEN",
-           default=os.environ.get('LINE_ACCESS_TOKEN'))
-)
+line_bot_api = LineBotApi(config("LINE_CHANNEL_ACCESS_TOKEN", default=os.environ.get('LINE_ACCESS_TOKEN')))
 # get LINE_CHANNEL_SECRET from your environment variable
-handler = WebhookHandler(
-    config("LINE_CHANNEL_SECRET",
-           default=os.environ.get('LINE_CHANNEL_SECRET'))
-)
+handler = WebhookHandler(config("LINE_CHANNEL_SECRET", default=os.environ.get('LINE_CHANNEL_SECRET')))
 
 
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
-
-
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
-
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
-
     return 'OK'
 
 
@@ -49,8 +32,6 @@ def handle_text_message(event):
         event.reply_token,
         TextSendMessage(text=event.message.text)
     )
-
-
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
